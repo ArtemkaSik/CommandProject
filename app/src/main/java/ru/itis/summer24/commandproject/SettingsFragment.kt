92e2f3a64d76791638a7af51fa31dc029ab42093
ArtemkaSik <153365212+ArtemkaSik@users.noreply.github.com>
2024-07-05 17:14:36 +0500

    Настройки приложения

--- app/src/main/java/ru/itis/summer24/commandproject/SettingsFragment.kt
+++ app/src/main/java/ru/itis/summer24/commandproject/SettingsFragment.kt
@@ -2,8 +2,10 @@ package ru.itis.summer24.commandproject
 
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
 import ru.itis.summer24.commandproject.databinding.FragmentSettingsBinding
+
 
 class SettingsFragment : Fragment(R.layout.fragment_settings) {
 
@@ -13,6 +15,35 @@ class SettingsFragment : Fragment(R.layout.fragment_settings) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentSettingsBinding.bind(view)
 
+        changeTheme()
+    }
+
+    private fun changeTheme(){
+        val currentTheme = getCurrentTheme()
+
+        binding?.switchTheme?.text = if (currentTheme == AppCompatDelegate.MODE_NIGHT_YES) {
+            "Dark Mode"
+        } else {
+            "Light Mode"
+        }
+
+        binding?.switchTheme?.setOnClickListener{
+            val newTheme = if (currentTheme == AppCompatDelegate.MODE_NIGHT_YES) {
+                AppCompatDelegate.MODE_NIGHT_NO
+            } else {
+                AppCompatDelegate.MODE_NIGHT_YES
+            }
+            AppCompatDelegate.setDefaultNightMode(newTheme)
+            binding?.switchTheme?.text = if (newTheme == AppCompatDelegate.MODE_NIGHT_YES) {
+                "Dark Mode"
+            } else {
+                "Light Mode"
+            }
+        }
+    }
+
+    private fun getCurrentTheme(): Int {
+        return AppCompatDelegate.getDefaultNightMode()
     }
 
     override fun onDestroyView() {
