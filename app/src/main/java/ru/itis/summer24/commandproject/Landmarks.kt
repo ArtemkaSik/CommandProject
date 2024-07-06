ca09e14ce71758acd2ea3d604c4a42a999483b96
enzhe <enzhe.ashrafullina@mail.ru>
2024-07-07 00:07:25 +0300

    Обновление: страница с культурными достопримечательностями

---
+++ app/src/main/java/ru/itis/summer24/commandproject/Landmarks.kt
@@ -0,0 +1,9 @@
+package ru.itis.summer24.commandproject
+
+data class Landmarks(
+    val id: Int,
+    val name: String,
+    val city: String,
+    val imageUrl: String,
+    val shortDescription: String,
+)
---
+++ app/src/main/java/ru/itis/summer24/commandproject/LandmarksAdapter.kt
@@ -0,0 +1,37 @@
+package ru.itis.summer24.commandproject
+
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.RequestManager
+import ru.itis.summer24.commandproject.databinding.ItemLandmarksBinding
+
+//Этот файл отвечает за отображение списка элементов в RecyclerView.
+
+class LandmarksAdapter (
+        private var list: List<Landmarks>,
+        private val glide: RequestManager,
+        private val onClick: (Landmarks) -> Unit
+    ) : RecyclerView.Adapter<LandmarksHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LandmarksHolder {
+            val binding = ItemLandmarksBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+            return LandmarksHolder(binding, glide, onClick)
+        }
+
+        override fun onBindViewHolder(holder: LandmarksHolder, position: Int) {
+            holder.onBind(list[position])
+        }
+
+        override fun getItemCount(): Int = list.size
+
+        fun updateDataset(newList: List<Landmarks>) {
+            list = newList
+            notifyDataSetChanged()
+        }
+    }
