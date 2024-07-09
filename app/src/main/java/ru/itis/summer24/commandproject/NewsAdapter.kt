516828571cf54b82fa9ebf2c001317b2c1b2b7d4
alexaaa-a <144144005+alexaaa-a@users.noreply.github.com>
2024-07-09 23:40:30 +0500

    new

--- app/src/main/java/ru/itis/summer24/commandproject/NewsAdapter.kt
+++ app/src/main/java/ru/itis/summer24/commandproject/NewsAdapter.kt
@@ -5,25 +5,36 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.RequestManager
 import ru.itis.summer24.commandproject.databinding.ItemNewsBinding
+import ru.itis.summer24.commandproject.models.New
 
 class NewsAdapter(
-    private val list: List<News>,
+    private var list: List<New>,
     private val glide: RequestManager,
-    private val onClick: (News) -> Unit,
+    private val onClick: (New) -> Unit,
 ) : RecyclerView.Adapter<NewsHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsHolder {
-        val binding = ItemNewsBinding.inflate(
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): NewsHolder = NewsHolder(
+        binding = ItemNewsBinding.inflate(
             LayoutInflater.from(parent.context),
             parent,
             false
-        )
-        return NewsHolder(binding, glide, onClick)
-    }
+        ),
+        glide = glide,
+        onClick = onClick,
+    )
 
     override fun onBindViewHolder(holder: NewsHolder, position: Int) {
         holder.onBind(list[position])
     }
 
     override fun getItemCount(): Int = list.size
+
+    fun updateDataset(newList: List<New>) {
+        list = newList
+        notifyDataSetChanged()
+    }
 }
